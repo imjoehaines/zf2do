adb1101a0e9ac6f2dc16c8da166476fd65c94d13
Joe Haines <hello@joehaines.co.uk>
2015-06-17 16:13:05 +0100

    Errors to twig templatesgs

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -85,8 +85,8 @@ return array(
         'template_map' => array(
             'layout/layout' => __DIR__ . '/../view/layout/layout.twig',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404' => __DIR__ . '/../view/error/404.phtml',
-            'error/index' => __DIR__ . '/../view/error/index.phtml',
+            'error/404' => __DIR__ . '/../view/error/404.twig',
+            'error/index' => __DIR__ . '/../view/error/index.twig',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
